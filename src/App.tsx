86fc7b9fa27eadc213aa9d0890ce21497e2d6925
BUHLE <buhlentombikhoza@gmail.com>
2026-07-19 13:17:19 +0200

    Initial commit, componet based framework already  initialized too

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import Navbar from "./components/Navbar/Navbar";
+import Sidebar from "./components/Sidebar/Sidebar";
+
+function App() {
+  return (
+    <div>
+      <Navbar/>
+      <Sidebar/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Navbar/Navbar.tsx
@@ -0,0 +1,60 @@
+import './Navbar.css';
+
+
+export default function Navbar() {
+    return(
+        <nav>
+            <div className="logo-area"> 
+                <div className="tooltip">   
+                    <span className="tooltip-text">Main Menu</span>
+                </div>
+                <img className="logo-img" src="https://www.gstatic.com/images/branding/productlogos/keep_2026/v2/web-48dp/logo_keep_2026_color_1x_web_48dp.png" />
+            </div>
+
+            <div className="search-area">
+            <div className="tooltip">
+                 <span className="material-symbols-outlined hover">search</span>
+                 <span className="tooltip-text">Search</span>
+            </div>
+                <input type="text" placeholder="Search" />
+
+
+        </div>
+
+        <div className="settings-area">
+            <div className="tooltip">
+                <span className="material-symbols-outlined hover">refresh</span>
+                <span className="tooltip-text">Refresh</span>
+            </div>
+
+            <div className="tooltip">
+                <span className="material-symbols-outlined hover">list</span>
+                <span className="tooltip-text">List view</span>
+            </div>
+
+            <div className="tooltip">
+                <span className="material-symbols-outlined hover">settings</span>
+                <span className="tooltip-text">Settings</span>
+            </div>
+
+        </div>
+
+        <div className="profile-actions-area">
+            <div className="tooltip">
+                <span className="material-symbols-outlined hover">apps</span>
+                <span className="tooltip-text">Google Apps</span>
+            </div>
+
+             <div className="tooltip">
+                <span className="material-symbols-outlined hover">account_circle</span>
+                <span className="tooltip-text">Accounts</span>
+            </div>
+
+
+        </div>
+
+
+        </nav>
+    );
+}
+
