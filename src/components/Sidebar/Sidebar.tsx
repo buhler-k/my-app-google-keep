b623f2c2168c5f55cd54ff8cf4482f77250fbebf
BUHLE <buhlentombikhoza@gmail.com>
2026-07-20 22:01:59 +0200

    Worked on the sidebar hover and display of the sidebar texts

--- src/components/Sidebar/Sidebar.tsx
+++ src/components/Sidebar/Sidebar.tsx
@@ -1,33 +1,81 @@
+import React, { useState } from 'react';
 import './Sidebar.css';
 
-export default function Sidebar(){
+const Sidebar = () => {
+    const [isHover, setIsHover] = useState(false);
+
+
+    const hoverOverHandler = () => {
+        setIsHover(true);
+
+    }
+
+    const hoverOutHandler = () => {
+        setIsHover(false);
+
+    }
+    const sidebarClickHandler = () => {
+        // placeholder click handler for sidebar
+    }
     return (
-        <div className="sidebar">
-            <div className="sidebar-item sidebar-active-item">
-                <span className="material-symbols-outlined hover active">lightbulb</span>
-                <span className="sidebar-text">Notes</span>
-            </div>
+
+        <div className="sidebar" onClick = {sidebarClickHandler} onMouseEnter = {hoverOverHandler} onMouseLeave={hoverOutHandler}>
+            
+                 <div className="sidebar-item sidebar-active-item">
+                    <span className="material-symbols-outlined hover active">lightbulb</span>
+                
+                    
+                    {isHover && (
+                    <div className="sidebar-item sidebar-active-item text">
+                        <span className="sidebar-text">Notes</span>
+                    </div>
+                        
+                    )} 
+                </div>
+                
+               
+        
 
             <div className="sidebar-item">
                 <span className="material-symbols-outlined hover">notifications</span>
-                <span className="sidebar-text">Reminders</span>
+                 {isHover && (
+                    
+                        <span className="sidebar-text">Reminders</span>
+                    )} 
+                
             </div>
 
             <div className="sidebar-item">
                 <span className="material-symbols-outlined hover ">edit</span>
-                <span className="sidebar-text">Edit Labels</span>
+
+                 {isHover && (
+                    
+                        <span className="sidebar-text">Edit Labels</span>
+                    )} 
+                
             </div>
 
             <div className="sidebar-item">
                 <span className="material-symbols-outlined hover ">archive</span>
-                <span className="sidebar-text">Archive</span>
+
+                 {isHover && (
+                    
+                        <span className="sidebar-text">Archive</span>
+                    )} 
+                
             </div>
 
             <div className="sidebar-item">
                 <span className="material-symbols-outlined hover ">delete</span>
-                <span className="sidebar-text">Trash</span>
+                 {isHover && (
+                    
+                        <span className="sidebar-text">Trash</span>
+                    )} 
+                
             </div>
         </div>
 
     );
-}
+};
+
+export default Sidebar;
